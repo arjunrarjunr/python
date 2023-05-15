f7102560872edf229e4677692a7cc8ab3e2b0701
arjun121094 <arjun121094@gmail.com>
2023-05-15 11:40:48 +0530

    All practices saved

---
+++ notebooks/cli.sh
@@ -0,0 +1,19 @@
+python3 \
+cli.py 1 \
+2 \
+3 \
+4
+
+
+python3 \
+cli.py 1 \
+2 \
+3 \
+4
+
+
+python3 \
+cli.py 1 \
+2 \
+3 \
+4
--- notebooks/hello.py
+++ notebooks/hello.py
@@ -1,3 +1,7 @@
-x=input()
-x="Hello "+x
-print(x)
+import pandas as pd
+x = ["Arjun","Aswin","Durga"]
+# x = sum(x)
+x = pd.DataFrame(x,columns=["Name"])
+print(x.Name.str.slice(0,3))
+
+
